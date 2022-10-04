0d513595913759a66ce8a3a77037b14d4f57005f
Alexey <alex-ey98@mail.ru>
2022-10-05 01:33:51 +0300

    Lesson_2_Method_array

---
+++ Lesson_2_method_array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1, 4, 5, 6, 3, 5, 4, 2, 6};
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index += 1;
+}
