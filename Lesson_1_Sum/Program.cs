a5e45b4a1e425ff914ebe92cd935055cac83716a
Alexey <alex-ey98@mail.ru>
2022-10-04 21:50:24 +0300

    Lesson_1_Sum_Random

--- Lesson_1_Sum/Program.cs
+++ Lesson_1_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int number_A = 31;
-int number_B = 5;
+﻿int number_A = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(number_A);
+int number_B = new Random().Next(1, 10);
+Console.WriteLine(number_B);
 int result = number_A + number_B;
 Console.WriteLine(result);
