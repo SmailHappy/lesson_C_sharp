60e6c3785527d956e01076c9cf106d7c3f712ddd
Alexey <alex-ey98@mail.ru>
2022-10-04 21:56:53 +0300

    Lesson_1_ifelse

---
+++ Lesson_1_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя епользователя: ");
+string user_name = Console.ReadLine();
+
+if (user_name.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(user_name);
+}
