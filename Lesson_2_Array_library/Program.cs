5389df74220232192d80f75ac0dc9855a3e064f6
Alexey <alex-ey98@mail.ru>
2022-10-05 01:44:32 +0300

    Lesson_2_array_lib

---
+++ Lesson_2_Array_library/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index += 1;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int positioin = 0;
+
+    while (positioin < count)
+    {
+        Console.WriteLine(col[positioin]);
+        positioin += 1;
+    }
+}
+
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index += 1;
+    }
+    
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 4);
+Console.WriteLine(pos);
