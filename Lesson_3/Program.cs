cef3e6a035c3033761cd3d4862151a98dcc81fc1
Alexey <alex-ey98@mail.ru>
2022-10-05 02:25:16 +0300

    Lesson_3_Text_format

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -47,11 +47,46 @@ string Method4(int count, string text)
 // string res = Method4(10, "asdf");
 // Console.WriteLine(res);
 
-for (int i = 2; i <= 10; i++)
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, " 
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
 {
-    for (int j = 2; j <= 10; j++)
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
     {
-        Console.WriteLine($"{i} * {j} = {i * j}");
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
-    Console.WriteLine();
+
+    return result;
 }
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'C', 'c');
+Console.WriteLine(newText);
+Console.WriteLine();
